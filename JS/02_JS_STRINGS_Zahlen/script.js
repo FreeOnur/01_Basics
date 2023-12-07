b89fc2a927c62172b6bdddeb9c95689473e244fc
FreeOnur <onur.arslan@student.htldornbirn.at>
2023-12-07 11:06:27 +0100

    02_JS_STRINGS_Zahlen  finished first task

---
+++ JS/02_JS_STRINGS_Zahlen/script.js
@@ -0,0 +1,33 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+
+// +, -, *, /
+
+let c = 7.4;
+let d = 7.5;
+
+result = c + d;
+console.log(result);
+
+// TEXTE STRINGS
+
+let name = "Andreas";
+
+console.log(name);
+console.log("Das Ergebnis von " +  a  + " und " + b + " ist: " + ab);
+
+
+
+//AUFGABEN
+
+let e = 17.4;
+let f = 3.9;
+
+result = e/f;
+
+console.log("The result of " + e + " divided by " + f + " is " + result);
+
+
