1355b11995d00e9cef2a39f60a6b86fdbd587d9e
FreeOnur <onur.arslan@student.htldornbirn.at>
2023-12-07 10:36:35 +0100

    01_Variablen_Operations finished the tasks(maths)

--- JS/01_Variablen_Operations/script.js
+++ JS/01_Variablen_Operations/script.js
@@ -4,12 +4,37 @@ console.log("Tolle Sache");
 //Variablen
 let a = 7;
 let b = 8;
-let c = a + b
+let c = a + b;
 
-console.log("A hat den Wert" + a)
+console.log("A hat den Wert" + a);
 
 a = 10;
-console.log("A hat den Wert" + a)
+console.log("A hat den Wert" + a);
 
-console.log("a+b=" + c)
+console.log("a+b=" + c);
+
+// Rechnungsarten
+// *, /, +, -
+c= b-a;
+
+console.log("b - a =" + c);
+
+
+//Aufgaben
+
+c = 10;
+
+let result_1 = a + b + c;
+let result_2 = (a + b)/c;
+let result_3 = c -a + b;
+let result_4 = a + b + c *3;
+let result_5 = (a + b + c)/4;
+
+console.log("a + b + c = " + result_1);
+console.log("(a+b)/c = " + result_2);
+console.log("c -a + b = " + result_3);
+console.log("a + b + c *3 = " + result_4);
+console.log("(a + b + c)/4 = " + result_5);
+
+
 
