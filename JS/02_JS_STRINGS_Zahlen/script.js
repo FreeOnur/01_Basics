20324b461b5435e00d781751d98ad66143785503
FreeOnur <onur.arslan@student.htldornbirn.at>
2023-12-07 11:09:55 +0100

    02_JS_STRINGS_Zahlen finished 2nd task

--- JS/02_JS_STRINGS_Zahlen/script.js
+++ JS/02_JS_STRINGS_Zahlen/script.js
@@ -21,7 +21,7 @@ console.log("Das Ergebnis von " +  a  + " und " + b + " ist: " + ab);
 
 
 
-//AUFGABEN
+//1) Aufgabe Berechne das Ergebnis von 17.4/3.9
 
 let e = 17.4;
 let f = 3.9;
@@ -30,4 +30,10 @@ result = e/f;
 
 console.log("The result of " + e + " divided by " + f + " is " + result);
 
+// 2) Aufgabe Lege eine Variable firstname und eine Variable lastname an. Weise den Variablen deinen Namen zu
+
+let firstname = "Onur"
+let lastname = "Arslan"
+
+console.log("Ich heiße " + firstname + " " + lastname + "!");
 
