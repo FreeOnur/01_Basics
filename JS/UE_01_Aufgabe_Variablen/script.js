bc91685e023b596071f5ebd62714bc0368b39514
FreeOnur <onur.arslan@student.htldornbirn.at>
2023-12-07 11:28:33 +0100

    UE_01_Aufgabe_Variablen Stunde ende Anzahl richtig

---
+++ JS/UE_01_Aufgabe_Variablen/script.js
@@ -0,0 +1,25 @@
+// Erstelle eine Variable banana mit dem Wert "Banane"
+let a = "Banane";
+// Erstelle eine Variable apple mit dem Wert "Apple"
+let b = "Apple";
+// Erstelle eine Variable bananaPricePerKilo mit dem Wert 2.14
+let bananaPricePerKilo = 2.14;
+// Erstelle eine Variable applePricePerKilo mit dem Wert 3.43
+let applePricePerKilo = 3.43;
+// Ein Apfel hat durchschnittlich ein Gewicht von 0.34 Kilo
+let apfelgewicht = 0.34;
+// Eine Banane hat durchschnittlich ein Gewicht von 0.22 Kilo
+let bananengewicht = 0.22;
+// Gib folgendes aus: Anzahl Bananen/Äpfel pro Kilo, Preis pro Banane/Apfel
+
+let apfelAnzahl = 1 / apfelgewicht;
+let bananenAnzahl = 1 / bananengewicht;
+
+
+console.log("Mann kann " + apfelAnzahl+ " Äpfel pro Kilo kaufen"); 
+console.log("Mann kann " + bananenAnzahl + " Bananen pro Kilo kaufen"); 
+let umform
+// Preis von 8 Äpfeln
+// Preis von 17 Bananen
+// Preis von 1 Tonne Äpfel
+// Preis von 1 Tonne Bananen
