ab35fa7ec4c4acd55b8ff762a6ccc8f17dd05cb5
FreeOnur <onur.arslan@student.htldornbirn.at>
2023-12-14 10:59:10 +0100

    04_Schleifen finished Aufgaben

--- JS/04_Schleifen/script.js
+++ JS/04_Schleifen/script.js
@@ -7,3 +7,31 @@ for (let count = 10; count>0; count--) {
     console.log( count + " Andreas");
 }
 
+// Zähle von 7 bis 45
+
+for (let i = 7; i<=45; i++) {
+    console.log(i + " Onur");
+}
+
+// Zähle von 9 bis 99
+
+for (let c = 9; c<= 99; c++){
+
+    console.log(c + "A")
+}
+
+// Zähle rückwärts von 10 bis 0
+
+for (let r = 10; r>=0; r--) {
+
+    console.log(r + " rückwärts")
+
+}
+
+// Zähle rückwärts von 1234 bis 1207
+
+for (let l = 1234; l>= 1207; l--) {
+
+    console.log(l + " Christmas")
+
+}
