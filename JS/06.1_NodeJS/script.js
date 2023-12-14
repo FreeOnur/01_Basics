b05c708a400e8710e2d71d76500750f9ececace1
FreeOnur <onur.arslan@student.htldornbirn.at>
2023-12-14 11:28:37 +0100

    06.1_NodeJS wrote the commands nodejs

---
+++ JS/06.1_NodeJS/script.js
@@ -0,0 +1,7 @@
+//cd ..                  Einen Ordner hoch
+//cd demo                In den Ordner dem
+//pwd                    zeigt in welchem Ordner ich bin
+//node script.js         führt script.js im aktuellen Ordner aus
+//ls                     Zeigt alle Files im aktuellen Directory
+//cd J (TAB)             Schlägt den passenden Ordner vor
+//Pfeil oben
