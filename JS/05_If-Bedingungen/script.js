0111eb0d6b2b867cc31d2cb6f4a32a5268385d45
FreeOnur <onur.arslan@student.htldornbirn.at>
2023-12-14 11:14:06 +0100

    05_If-Bedingungen Finished Aufgabe 1

--- JS/05_If-Bedingungen/script.js
+++ JS/05_If-Bedingungen/script.js
@@ -1,9 +1,29 @@
 
-let number = 28;
-if (number < 25) {
-    console.log(number);
-} else if(number<50){
-    console.log("kleiner 50 " + number);
+let number1 = 28;
+if (number1 < 25) {
+    console.log(number1);
+} else if(number1<50){
+    console.log("kleiner 50 " + number1);
 } else {
     console.log("else");
 }
+
+// erstelle eine Variable number
+let number = 51;
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+if(number<20) {
+    console.log("Mini")
+} else if(number<50) {
+    console.log("Medium")
+}else{
+    console.log("Large")
+}
+
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+// Wenn die Zahl größer als 90 ist gib aus "Large"
