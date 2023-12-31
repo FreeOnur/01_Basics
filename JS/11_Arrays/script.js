e5da4f0b2d82d0489a874071ffe90a38377b55d3
FreeOnur <onur.arslan@student.htldornbirn.at>
2023-12-31 15:11:26 +0100

    11_Arrays added finished Aufgabe 1, 2 ,3

--- JS/11_Arrays/script.js
+++ JS/11_Arrays/script.js
@@ -12,3 +12,20 @@ for (let index = 0; index < arr.length; index++) {
 
 arr.push(10);
 arr.push(3);
+
+//Erstelle ein Array mit folgendne Werten:
+//4,1,2,3
+
+let array = [4,1,2,3];
+
+//Füge die Werte 17 und 199 dazu
+array.push(17, 199);
+
+//a Gib alle Werte in einer Schleife aus!
+for (let index = 0; index < array.length; index++) {
+    console.log(index);
+    
+}
+//b Zähle alle Werte zusammen und gib das Ergebnis aus
+
+//c Berechne den Mittelwert der Zahlen und gib das Ergebnis aus
