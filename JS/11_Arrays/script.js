194ce018fd9224edc22980b2d346d475c0e72246
FreeOnur <onur.arslan@student.htldornbirn.at>
2023-12-31 15:06:29 +0100

    11_Arrays added finished tutorial

---
+++ JS/11_Arrays/script.js
@@ -0,0 +1,14 @@
+let a = 4;
+let b = 9;
+let c = 8;
+
+let arr = [4,9,8];
+console.log(arr[1]);
+
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index]);
+    
+}
+
+arr.push(10);
+arr.push(3);
