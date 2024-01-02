54b93e685a975aad219c1757e2b36c37af54b248
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-01-02 20:01:05 +0100

    13_Bankomat error muss readline close selber setzen

--- JS/13_Bankomat/script.js
+++ JS/13_Bankomat/script.js
@@ -9,7 +9,7 @@ const readLineAsync = () => {
     return new Promise((resolve) => {
         readline.question("", (userRes) => {
             resolve(userRes);
-            readline.close();
+            
         });
     });
 };
@@ -18,3 +18,17 @@ console.log("1. Einzahlen");
 console.log("2. Abheben");
 console.log("3. Kontostand");
 console.log("4. Ende");
+let userFirstInput = await readLineAsync();
+switch(userFirstInput) {
+    case "1":
+        console.log("Geben Sie den Betrag ein den Sie einzahlen möchten:");
+        let userEinzahlenInput = await readLineAsync();
+        console.log("Sie haben " + userEinzahlenInput + "€ eingezahlt");
+    break;
+    case 2 : 
+        console.log("Wie viel würden Sie gerne abheben?");
+        let userAbhebenInput = await readLineAsync();
+        console.log("Sie haben " + userAbhebenInput + " abgehoben");
+    break;
+}
+readline.close();
