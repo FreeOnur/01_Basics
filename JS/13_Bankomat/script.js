b52f4fb6b1ec91f58418d5fb3ad544a31208cef9
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-01-02 20:31:02 +0100

    13_Bankomat changed code but error still there

--- JS/13_Bankomat/script.js
+++ JS/13_Bankomat/script.js
@@ -9,6 +9,7 @@ const readLineAsync = () => {
     return new Promise((resolve) => {
         readline.question("", (userRes) => {
             resolve(userRes);
+            readline.close();
             
         });
     });
@@ -19,16 +20,23 @@ console.log("2. Abheben");
 console.log("3. Kontostand");
 console.log("4. Ende");
 let userFirstInput = await readLineAsync();
-switch(userFirstInput) {
-    case "1":
-        console.log("Geben Sie den Betrag ein den Sie einzahlen möchten:");
-        let userEinzahlenInput = await readLineAsync();
-        console.log("Sie haben " + userEinzahlenInput + "€ eingezahlt");
-    break;
-    case 2 : 
-        console.log("Wie viel würden Sie gerne abheben?");
-        let userAbhebenInput = await readLineAsync();
-        console.log("Sie haben " + userAbhebenInput + " abgehoben");
-    break;
+if(userFirstInput == 1) {
+    console.log("Geben Sie den Betrag ein den Sie einzahlen möchten:");
+    let userEinzahlenInput = await readLineAsync();
+    console.log("Sie haben " + userEinzahlenInput + "€ eingezahlt");
+    
 }
-readline.close();
+if(userFirstInput == 2) {
+    console.log("Wie viel würden Sie gerne abheben");
+    let userAbhebenInput = await readLineAsync();
+    console.log("Sie haben " + userAbhebenInput + "€ ausgezahlt");
+   
+
+}
+
+
+if(userFirstInput == 3) {
+    
+    
+    console.log("Der Kontostand beträgt: " + userBasicStand)
+}
