9ec0d027dbab23b36b9b10c51e74618877b8c2e9
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-01-02 23:20:47 +0100

    14.2_TikTakToe made function printField

--- JS/14.2_TikTakToe/script.js
+++ JS/14.2_TikTakToe/script.js
@@ -3,16 +3,28 @@ let field = [
     [0,0,0],
     [0,0,0]
 ];
+printField();
 
 
-for (let row = 0; row < field.length; row++) {
-    let actualRow = field[row];
-    let output = "";
-    for (let col = 0; col < actualRow.length; col++) {
-        
-        output += actualRow[col] + " ";
-        
+field[0][1] = 1;
+
+printField();
+function printField(){
+    
+    
+    
+    for (let row = 0; row < field.length; row++) {
+        let actualRow = field[row];
+        let output = "";
+        for (let col = 0; col < actualRow.length; col++) {
+            
+            output += actualRow[col] + " ";
+            
+        }
+       
+        console.log(output);
     }
-   
-    console.log(output);
+    
+    
+    
 }
