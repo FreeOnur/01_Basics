0f12ae746a30970cbf4328f1864bd823c00aa680
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-01-02 22:46:54 +0100

    13_Bankomat changed to version with switch and add while

--- JS/13_Bankomat/script.js
+++ JS/13_Bankomat/script.js
@@ -16,30 +16,28 @@ const readLineAsync = () => {
     
 };
 
+let bankautomat = true;
 
 console.log("1. Einzahlen");
 console.log("2. Abheben");
 console.log("3. Kontostand");
 console.log("4. Ende");
+let userBasicStand = 0;
+
 let userFirstInput = await readLineAsync();
-if(userFirstInput == 1) {
+switch(userFirstInput) {
+    case "1":
     console.log("Geben Sie den Betrag ein den Sie einzahlen möchten:");
     let userEinzahlenInput = await readLineAsync();
     console.log("Sie haben " + userEinzahlenInput + "€ eingezahlt");
-    
-    
-}
-if(userFirstInput == 2) {
-    console.log("Wie viel würden Sie gerne abheben");
-    let userAbhebenInput = await readLineAsync();
-    console.log("Sie haben " + userAbhebenInput + "€ ausgezahlt");
-   
+    userBasicStand += parseFloat(userEinzahlenInput);
+    console.log(userBasicStand);
 
-}
-
-
-if(userFirstInput == 3) {
-    
-    
-    console.log("Der Kontostand beträgt: " + userBasicStand)
+    break;
+    case "2":
+        console.log("Geben Sie den Betrag ein den Sie auszahlen möchten:");
+        let userAuszahlenInput = await readLineAsync();
+        console.log("Sie haben" + userAuszahlenInput + "€ ausgezahlt");
+        userBasicStand -= userAuszahlenInput;
+        break;
 }
