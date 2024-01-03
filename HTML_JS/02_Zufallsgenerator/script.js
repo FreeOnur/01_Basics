2bd8a453c2465739a7734a11906b0191cc9da8ea
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-01-03 19:33:57 +0100

    02_Zufallsgenerator  finished Randomizer

--- HTML_JS/02_Zufallsgenerator/script.js
+++ HTML_JS/02_Zufallsgenerator/script.js
@@ -2,10 +2,15 @@
 function buttonClicked() {
 let x = document.getElementById("num1").value;
 let y = document.getElementById("num2").value;
+let output = document.getElementById("output");
+
 let ZahlX = parseInt(x)
 let ZahlY = parseInt(y)
+
+
 let random = Math.random()* ZahlY + ZahlX;
-console.log(random);
+console.log(Math.floor(random));
+output.innerHTML = Math.floor(random);
 
 
 }
