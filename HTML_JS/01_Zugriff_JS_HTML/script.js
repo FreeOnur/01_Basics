28531a3e66604ef1010c7698ffe980f29f54ac3b
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-01-03 19:54:09 +0100

    01_Zugriff_JS_HTML made an output

--- HTML_JS/01_Zugriff_JS_HTML/script.js
+++ HTML_JS/01_Zugriff_JS_HTML/script.js
@@ -2,28 +2,30 @@ function buttonClicked() {
     let num1 = document.getElementById("number1").value;
     let num2 = document.getElementById("number2").value;
     let operator = document.getElementById("Operator").value;
+    let output = document.getElementById("output")
     switch(operator) {
         case "%":
            let result= parseInt(num1) % parseInt(num2);
-           console.log(result);
+           output.innerHTM = result
             break;
         case "/":
            let result1= parseInt(num1)/ parseInt(num2);
-            console.log(result1);
+           output.innerHTML = result1;
             break;
         case "+":
            let result2= parseInt(num1)+ parseInt(num2);
-            console.log(result2);
+           output.innerHTML = result2;
             break;
         case "-":
             let result3= parseInt(num1)- parseInt(num2);
-            console.log(result3);
+            output.innerHTML = result3;
             break;
 
         case "+":
                let result4= parseInt(num1)* parseInt(num2)
-                console.log(result4);
+               output.innerHTML = result4;
                 break;
     }
+    
     
 }
