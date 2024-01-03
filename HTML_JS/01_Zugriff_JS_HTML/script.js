7563c4a184831ee2cff0b4930eb6622649760652
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-01-03 15:03:40 +0100

    01_Zugriff_JS_HTML made small calculator with more operators

--- HTML_JS/01_Zugriff_JS_HTML/script.js
+++ HTML_JS/01_Zugriff_JS_HTML/script.js
@@ -1,3 +1,29 @@
-let element = document.getElementById("content").innerHTML = "Tolle Sache";
+function buttonClicked() {
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+    let operator = document.getElementById("Operator").value;
+    switch(operator) {
+        case "%":
+           let result= parseInt(num1) % parseInt(num2);
+           console.log(result);
+            break;
+        case "/":
+           let result1= parseInt(num1)/ parseInt(num2);
+            console.log(result1);
+            break;
+        case "+":
+           let result2= parseInt(num1)+ parseInt(num2);
+            console.log(result2);
+            break;
+        case "-":
+            let result3= parseInt(num1)- parseInt(num2);
+            console.log(result3);
+            break;
 
-console.log(element);
+        case "+":
+               let result4= parseInt(num1)* parseInt(num2)
+                console.log(result4);
+                break;
+    }
+    
+}
