451f4e954cced0038f34b33d8b86200d0eaf8b37
BuildTools <unconfigured@null.spigotmc.org>
2024-01-04 19:58:26 +0100

    03_JSON two variants of loops forEach and for

--- HTML_JS/03_JSON/script.js
+++ HTML_JS/03_JSON/script.js
@@ -1,8 +1,22 @@
 
-let person = {firstName:"Hans", lastName:"Huber", points:30}
+let data = [
+    {firstName:"Hans", lastName:"Huber", points:30},
+    {firstName:"Paul", lastName:"Müller", points:35},
+    {firstName:"Franz", lastName:"Maier", points:32},
+]
 
-console.log(person.firstName);
 
+
+
+for (let index = 0; index < data.length; index++) {
+    const element = data[index];
+    console.log(element.firstName);
+    
+}
+
+data.forEach(element => {
+    console.log(element.lastName);
+})
 function loadPeople() {
 
 
