fcc1ea0d1e308a6813cb109a42306eccb060488f
BuildTools <unconfigured@null.spigotmc.org>
2024-01-06 22:59:32 +0100

    05_Projekt added numbers

--- HTML_JS/05_Projekt/script.js
+++ HTML_JS/05_Projekt/script.js
@@ -63,3 +63,16 @@ let lettersLow = [
     {letter: "ü"}
 
 ]
+let numbers = [
+    {letter: "1"},
+    {letter: "2"},
+    {letter: "3"},
+    {letter: "4"},
+    {letter: "5"},
+    {letter: "6"},
+    {letter: "7"},
+    {letter: "8"},
+    {letter: "9"},
+    {letter: "0"}
+
+]
