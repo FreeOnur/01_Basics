ed2969a219e868a7ca179f56fd5590da1d72ec3a
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-01-11 10:44:32 +0100

    06_UE_Magic function magic

--- HTML_JS/06_UE/script.js
+++ HTML_JS/06_UE/script.js
@@ -1,4 +1,20 @@
 let  größe = document.getElementById("größe").value;
 let gewicht = document.getElementById("gewicht").value;
+function magic(größe, gewicht) {
+    let magic = ((größe*1.43)/gewicht)- 3.4;
+    return magic
+}
+console.log(magic(2, 3));
 
-let magic = ((größe*1.43)/gewicht)- 3.4
+
+if(magic > 0) {
+    document.getElementById("output").innerHTML = "Positiv";
+    for(let i = 0; i >=magic; i++) {
+        document.getElementById("output").innerHTML = i;
+    }
+} else if ( magic < 0) {
+    document.getElementById("output").innerHTML = "Negativ";
+    for(let i = 0; i <=magic; i--) {
+        document.getElementById("output").innerHTML = i;
+    }
+}
