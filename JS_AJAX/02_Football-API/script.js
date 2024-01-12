e90fd962761fda05e7b564ddf560c0f28123e79f
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-01-12 20:10:43 +0100

    02_Footbal-API finished

--- JS_AJAX/02_Football-API/script.js
+++ JS_AJAX/02_Football-API/script.js
@@ -1,4 +1,20 @@
 const loadData = () => {
-    fetch("https://api.openligadb.de/getbltable/bl1/2022")
+    fetch("https://api.openligadb.de/getbltable/bl1/2022").then((result) => {
+        result.json().then((data) => {
+            console.log(data);
+            fillTable(data);
+        })
+    })
+}
+
+const fillTable = (data) => {
+   
+    let html ="";
+
+    data.forEach(element => {
+        html += "<div>" + element.points + " -- "+element.teamName + "</div>";
+    });
+    document.getElementById("output").innerHTML = html;
+
 }
 loadData();
