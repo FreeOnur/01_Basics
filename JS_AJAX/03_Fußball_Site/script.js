6e8f0a3339d6deee978e112ccd9a1046fff6c79d
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-01-29 20:52:55 +0100

    03_Fußball_Site testing code

--- JS_AJAX/03_Fußball_Site/script.js
+++ JS_AJAX/03_Fußball_Site/script.js
@@ -1,6 +1,16 @@
 const loadData = () => {
     fetch("https://api.openligadb.de/getbltable/bl1/2023").then((result) => {
         result.json().then((data) => {
+            getInfos(data);
+
         })
     })
+}
+
+const getInfos = (data) => {
+    data.forEach(element => {
+        html += "<ul> <il>" + element.points + "</il> </ul>"
+    });
+
+    document.getElementById("output").innerHTML = html;
 }
