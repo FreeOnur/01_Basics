c0aa275bc4ca683ecafc3fbac62b721fe818f2d3
BuildTools <unconfigured@null.spigotmc.org>
2024-02-08 22:42:38 +0100

    03_Fußball_Site added JSON arrays

--- JS_AJAX/03_Fußball_Site/script.js
+++ JS_AJAX/03_Fußball_Site/script.js
@@ -1,4 +1,31 @@
+let jahre = [
+    {id:2002, value:"2002"},
+    {id:2003, value:"2003"},
+    {id:2004, value:"2004"},
+    {id:2005, value:"2005"},
+    {id:2006, value:"2006"},
+    {id:2007, value:"2007"},
+    {id:2008, value:"2008"},
+    {id:2009, value:"2009"},
+    {id:2010, value:"2010"},
+    {id:2011, value:"2011"},
+    {id:2012, value:"2012"},
+    {id:2013, value:"2013"},
+    {id:2014, value:"2014"},
+    {id:2015, value:"2015"},
+    {id:2016, value:"2016"},
+    {id:2017, value:"2017"},
+    {id:2018, value:"2018"},
+    {id:2019, value:"2019"},
+    {id:2020, value:"2020"},
+    {id:2021, value:"2021"},
+    {id:2022, value:"2022"},
+    {id:2023, value:"2023"},
+]
 const loadData = () => {
+    let jahre = document.getElementById("year").value
+    let jahreId = 
+
     fetch("https://api.openligadb.de/getbltable/bl1/2023").then((result) => {
         result.json().then((data) => {
             getInfos(data);
@@ -10,7 +37,7 @@ const loadData = () => {
 const getInfos = (data) => {
     let html = "";
     data.forEach(element => {
-        //html += "<table><tr><td>" + element.shortName + " </td> <td> hallo </td></tr> " + "<tr> <th>"+ element.goals +"</th> </tr> </table>"
+        html += "<table><tr><td>" + element.shortName + " </td> <td> </td></tr> " + "<tr> <th>"+ element.goals +"</th> </tr> </table>"
     });
     
     
