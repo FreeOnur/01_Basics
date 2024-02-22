97dc673bd010424f011b810e7c139af0d601ed86
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-02-22 11:30:43 +0100

    08_JS_Scope_M Stunde finished

---
+++ Javascript Extended/08_JS_Scope_M/script.js
@@ -0,0 +1,15 @@
+if (true) {
+    var varVariable = "This is true"
+}
+
+console.log(varVariable);
+if(true) {
+     letVariable = "This is true"
+    console.log(letVariable);
+}
+console.log(letVariable);
+if(true) {
+    const constVariable = "This is true"
+    console.log(constVariable);
+}
+console.log(constVariable);
