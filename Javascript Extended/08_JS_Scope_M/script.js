0278ccf16a0acf533dedc6ff2dd44024b20a6447
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-02-22 11:31:04 +0100

    08_JS_Scope_M Stunde finished

--- Javascript Extended/08_JS_Scope_M/script.js
+++ Javascript Extended/08_JS_Scope_M/script.js
@@ -2,14 +2,22 @@ if (true) {
     var varVariable = "This is true"
 }
 
+
+var varVariable = "This is false"
 console.log(varVariable);
-if(true) {
-     letVariable = "This is true"
-    console.log(letVariable);
-}
+let letVariable = "true"
+
+//bei erstellung von var variablen in einem code block wie zum beispiel If
 console.log(letVariable);
-if(true) {
-    const constVariable = "This is true"
-    console.log(constVariable);
-}
-console.log(constVariable);
+
+// console.log(varVariable);
+// if(true) {
+//      letVariable = "This is true"
+//     console.log(letVariable);
+// }
+// console.log(letVariable);
+// if(true) {
+//     const constVariable = "This is true"
+//     console.log(constVariable);
+// }
+// console.log(constVariable);
