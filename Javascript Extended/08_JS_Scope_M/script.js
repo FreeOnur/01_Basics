7411d87101ccce4e85d086aacd5bd1d55b9c61e8
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-02-29 10:31:02 +0100

    08_JS_Scope_M added some notes and finished

--- Javascript Extended/08_JS_Scope_M/script.js
+++ Javascript Extended/08_JS_Scope_M/script.js
@@ -2,6 +2,10 @@ if (true) {
     var varVariable = "This is true"
 }
 
+// let beispiel = 1;
+// let beispiel = 4;
+
+//Bei var Variablen kann man erneut die Werte definieren und bei letvariablen kann man sie nicht nochmal definieren
 
 var varVariable = "This is false"
 console.log(varVariable);
@@ -21,3 +25,25 @@ console.log(letVariable);
 //     console.log(constVariable);
 // }
 // console.log(constVariable);
+
+
+
+//var Variablen kann man auch nachdem man sie aufgerufen hat erst erstellen jedoch werden sie undefined
+//const und let sind fast gleich, der einzige Unterschied bei const ist dass man const nicht erneut definieren kann
+
+console.log(varVariable2);
+var varVariable2 = 2;
+console.log(varVariable2);
+
+
+
+// console.log(letvar);
+// let letvar = 2;
+// console.log(letvar);
+
+
+// man kann const arrays/json bearbeiten und erneut definieren: 
+const constArr = {name: "Bob"}
+
+constArr.name = "sally"
+console.log(constArr);
