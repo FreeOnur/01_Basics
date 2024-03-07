14fcfc0b89a508a0379624816557afb30d97c854
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-03-07 11:02:44 +0100

    09_JS_CleanCode finished

---
+++ Javascript Extended/09_JS_Cleancode/script.js
@@ -0,0 +1,11 @@
+//bei == ist es egal ob es ein string oder Zahl ist jedoch ist es bei === nicht egal. Es schaut welcher Werttyp es ist
+if(1==="1"){
+    console.log(true);
+}
+
+var l = 21
+
+if(21=== l) {
+   console.log("hallo")
+}
+
