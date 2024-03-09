d024ede448cb61863edf7c9b410aaede23b580d1
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-03-09 11:23:24 +0100

    10_JS_Arrow_functions finished

--- Javascript Extended/10_JS_Arrow_functions/script.js
+++ Javascript Extended/10_JS_Arrow_functions/script.js
@@ -1,26 +1,26 @@
-// function sum(a, b) {
-//     return a + b
-// }
-// let sum2 = (a, b) => a + b
+function sum(a, b) {
+    return a + b
+}
+let sum2 = (a, b) => a + b
 
 
 
-// function isPositive(number) {
-//     return number >= 0
-// }
-// let isPositive2 = number => number >= 0
+function isPositive(number) {
+    return number >= 0
+}
+let isPositive2 = number => number >= 0
 
 
-// function randomNumber() {
-//     return Math.random
-// }
-// let randomNumber2 = () => Math.random
+function randomNumber() {
+    return Math.random
+}
+let randomNumber2 = () => Math.random
 
-// document.addEventListener('click', function() {
-//     console.log('click');
-// })
+document.addEventListener('click', function() {
+    console.log('click');
+})
 
-// document.addEventListener('click', () => console.log('click'))
+document.addEventListener('click', () => console.log('click'))
 
 
 
@@ -42,3 +42,6 @@ class Person {
     }
 }
 
+let person = new Person('Bob')
+person.printNameArrow()
+person.printNameFunction
