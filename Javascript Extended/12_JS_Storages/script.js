342c910460b35ee638c19cfd750a7f533f81a912
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-03-14 11:08:53 +0100

    12_JS_Storages finished

--- Javascript Extended/12_JS_Storages/script.js
+++ Javascript Extended/12_JS_Storages/script.js
@@ -1,6 +1,12 @@
 localStorage.setItem('name', 'Bob')
+console.log(localStorage.getItem('name'))
 localStorage.removeItem('name')
 
 sessionStorage.setItem('name', 'John')
 sessionStorage.setItem('name', 'Dawg')
 sessionStorage.removeItem('name')
+
+
+document.cookie = 'name=Mike; expires=' + new Date(2025, 0, 1).toUTCString()
+document.cookie = 'lastname=Tyson; expires=' + new Date(2025, 0, 1).toUTCString()
+console.log(document.cookie);
