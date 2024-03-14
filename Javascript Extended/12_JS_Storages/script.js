48093903ee710613759b0a747d683a0b869ad6b9
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-03-14 11:02:01 +0100

    12_JS_Storages session storage and localStorage functions

---
+++ Javascript Extended/12_JS_Storages/script.js
@@ -0,0 +1,6 @@
+localStorage.setItem('name', 'Bob')
+localStorage.removeItem('name')
+
+sessionStorage.setItem('name', 'John')
+sessionStorage.setItem('name', 'Dawg')
+sessionStorage.removeItem('name')
