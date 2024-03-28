50723fdafa4dc345bf2f0240ea8c1351b993bcf3
BuildTools <unconfigured@null.spigotmc.org>
2024-03-28 19:47:16 +0100

    01_Tailwind_Project_1 finished filter and cards overall and added new feature to filter jobtitle and names switch

--- REACT/01_Tailwind_Project_1/cards/src/components/PeopleContainer.js
+++ REACT/01_Tailwind_Project_1/cards/src/components/PeopleContainer.js
@@ -4,17 +4,46 @@ import Card_v2 from './Card_v2';
 
 export default function PeopleContainer() {
 const [people, setPeople] = useState([])
+const [filteredpeople, setFilteredPeople] = useState([])
+
 useEffect(() => {
    fetch("https://660576fd2ca9478ea180618d.mockapi.io/Person").then(res => res.json().then(data=> {
     setPeople(data);
+    setFilteredPeople(data)
    })) 
 },[])
+const filterpeople = (filter) => {
+    var filtered = people.filter(person => person.name.includes(filter))
+    document.getElementById("butt").addEventListener("click", () => {
+     filtered = people.filter(person =>person.jobTitle.includes(filter))
+     document.getElementById("butt").innerHTML = "JobTitel" 
+     setFilteredPeople(filtered);
+    })
+    setFilteredPeople(filtered)
+
+ }
+
+    
+
   return (
     <div>
       <h1>People</h1>
+        <div className='border'>
+            <button id='butt' className="border relative left-96">
+                      Name
+                    </button>
+            <input className=' border p-6'
+              type="text"
+              placeholder='Search'
+                
+              onChange={(element)=>{
+                    console.log(element.target.value);
+                    filterpeople(element.target.value);
+              }}/>
+        </div>
       <div className=' grid grid-cols-4 lg:grid-cols-4 gap-2'>
-        {people.map(element => {
-            return <Card_v2 name={element.firstName} title={element.jobTitle} imageUrl={element.avatar}/>
+        {filteredpeople.map(element => {
+            return <Card_v2 name={element.name} title={element.jobTitle} imageUrl={element.avatar}/>
         })}
       </div>
     </div>
