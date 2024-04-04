d6722afbbf4d3299dcfffba7e6fb9116d6a4e771
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-04-04 11:28:08 +0200

    02_Router stunde finished

--- REACT/02_Router/src/App.js
+++ REACT/02_Router/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import friends from './components/pages/friends';
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/friends",
+    element: <people />,
+  },
+  {
+    path: "/People",
+    element: <friends />,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <friends />
+
+  return <RouterProvider router={router} />
 }
 
 export default App;
--- REACT/02_Router/src/components/pages/friends.js
+++ REACT/02_Router/src/components/pages/friends.js
@@ -1,9 +1,8 @@
 import React from 'react'
 
 export default function friends() {
-    return (
-        <div>
-            friends
-        </div>
-    )
+    return <div>
+        friends
+    </div>
+
 }
