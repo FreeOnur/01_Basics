6c3e31b4174a739240e8a9110fa57da826378949
BuildTools <unconfigured@null.spigotmc.org>
2024-04-09 21:31:42 +0200

    02_Router Router Pass data tested out how to work with that

--- REACT/02_Router/src/components/pages/Person.js
+++ REACT/02_Router/src/components/pages/Person.js
@@ -4,7 +4,10 @@ import { Link, useLocation, useNavigate } from 'react-router-dom/dist/umd/react-
 export default function Person() {
   const location = useLocation();
   const navigate = useNavigate();
-  console.log(location.state);
+  let arr = location.state
+  if(arr.age <= 18) {
+    console.log("Hans is " + arr.age + " years old")
+  }
   return (
     <div>
       <h1>Hello Friends</h1>
