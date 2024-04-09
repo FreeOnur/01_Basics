89c673057e69b2f2f95b9dbf502bc3d20bcf8ab9
BuildTools <unconfigured@null.spigotmc.org>
2024-04-09 21:25:53 +0200

    02_Router Router Pass data finished the video testing out

--- REACT/02_Router/src/components/pages/People.js
+++ REACT/02_Router/src/components/pages/People.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom/dist/umd/react-router-dom.development';
 export default function People() {
   return <div>
     <h1>Hello People</h1>
-    <Link to="/"> Person </Link>
+    <Link to="/" state={{name:"Hans", age:17}}> Person </Link>
 
 
   </div >;
--- REACT/02_Router/src/components/pages/Person.js
+++ REACT/02_Router/src/components/pages/Person.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Link, useNavigate } from 'react-router-dom/dist/umd/react-router-dom.development';
+import { Link, useLocation, useNavigate } from 'react-router-dom/dist/umd/react-router-dom.development';
 
 export default function Person() {
+  const location = useLocation();
   const navigate = useNavigate();
+  console.log(location.state);
   return (
     <div>
       <h1>Hello Friends</h1>
