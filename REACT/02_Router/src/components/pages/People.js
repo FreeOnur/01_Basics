790924a7a6b23693796e58de2fe48af96657fbf6
BuildTools <unconfigured@null.spigotmc.org>
2024-04-09 22:49:17 +0200

    02_Router change Font finished

--- REACT/02_Router/src/components/pages/People.js
+++ REACT/02_Router/src/components/pages/People.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom/dist/umd/react-router-dom.development';
 
 export default function People() {
   return <div>
-    <h1>Hello People</h1>
+    <h1 className='text-xl font-bungee'>Hello People</h1>
     <Link to="/" state={{name:"Hans", age:17}}> Person </Link>
 
 
