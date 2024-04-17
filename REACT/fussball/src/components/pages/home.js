a294fdb12e2362fe585b2974fdc595e2e0b6f090
BuildTools <unconfigured@null.spigotmc.org>
2024-04-17 21:45:33 +0200

    fußball changed name of link text to Tabelle

--- REACT/fussball/src/components/pages/home.js
+++ REACT/fussball/src/components/pages/home.js
@@ -4,7 +4,7 @@ import Slidebar from '../slidebar'
 
 export default function Home() {
   return (<div>
-    <div className=' w-32 h-56 text-[]'><Slidebar link={"./table"} name="Jo" /></div>
+    <div className=' w-32 h-56 text-[]'><Slidebar link={"./table"} name="Tabelle" /></div>
     
     </div>
   )
