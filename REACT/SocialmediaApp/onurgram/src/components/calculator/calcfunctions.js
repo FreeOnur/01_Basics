1b96aabfa4999cde6c7112ac68a9a04947b0ddf0
BuildTools <unconfigured@null.spigotmc.org>
2024-05-02 21:03:55 +0200

    Multifunktionsapp namens onurgram gerade am taschenrechner dran

---
+++ REACT/SocialmediaApp/onurgram/src/components/calculator/calcfunctions.js
@@ -0,0 +1,3 @@
+function calc() {
+    let rechnung = document.getElementById("")
+}
---
+++ REACT/SocialmediaApp/onurgram/src/components/sidebar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <ul>
+        <li>
+        <span>Taschenrechner</span>
+        </li>
+
+
+        <li>
+        <span>Converter</span>
+        </li>
+
+
+        <li>
+        <span>Ai</span>
+        </li>
+
+
+        <li>
+        <span>Weather</span>
+        </li>
+
+
+        <li>
+        <span>Taschenrechner</span>
+        </li>
+
+    </ul>
+  )
+}
