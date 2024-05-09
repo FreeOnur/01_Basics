d0127e737d2d26ca6ccdd0bde032b2d4c51bd04a
BuildTools <unconfigured@null.spigotmc.org>
2024-05-09 15:32:12 +0200

    Multifunktions added transitions to slidebar and changed to more props

--- REACT/SocialmediaApp/onurgram/src/components/pages/Home.js
+++ REACT/SocialmediaApp/onurgram/src/components/pages/Home.js
@@ -2,8 +2,9 @@ import React from 'react'
 import Sidebar from '../sidebar'
 
 export default function Home() {
+  
   return (
-    <div className=' bg-[#2B2B2B] h-[1020px] w-full'><Sidebar/></div>
+    <div className=' bg-[#2B2B2B] h-[1020px] w-full'><Sidebar prop1='Taschenrechner' prop2='Converter' prop3='CSS-Converter' prop4='CodeTester'/></div>
     
   )
 }
--- REACT/SocialmediaApp/onurgram/src/components/sidebar.js
+++ REACT/SocialmediaApp/onurgram/src/components/sidebar.js
@@ -1,32 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import menu from './images/speisekarte.png'
 
-export default function Sidebar() {
+export default function Sidebar( props ) {
+  const [isOpen, setIsOpen] = useState(true)
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen); 
+  };
+
   return (
-    <ul className='bg-white w-[320px] '>
-        <li>
-        <span className='text-[32px] drop-shadow-[10px] text-shadow'>Taschenrechner</span>
-        </li>
 
-
-        <li>
-        <span className='text-[32px]'>Converter</span>
-        </li>
-
-
-        <li>
-        <span className='text-[32px]'>Ai</span>
-        </li>
-
-
-        <li>
-        <span className='text-[32px]'>Weather</span>
-        </li>
-
-
-        <li>
-        <span className='text-[32px]'>Taschenrechner</span>
-        </li>
-
+    <div>
+     
+    <ul className={isOpen ? ' fixed bg-white w-[320px] h-[1920px] duration-300 translate-x-0' : ' relative duration-300 translate-x-[-275px] bg-white w-[320px] h-[1920px] '} >
+    <div>
+            <img onClick={toggleSidebar} className={isOpen ? 'size-5 relative left-2 top-2' : ' size-5 relative left-[285px] top-2 '} src={menu} alt='menu'/>
+            </div>
+          {Object.entries(props).map(([propNamem, propValue], index)=> (
+            <li key={index} className='mb-[20px] '>
+            <span className='text-[25px] drop-shadow-[10px] text-shadow'>{propValue}</span>
+            </li>
+          ))}
     </ul>
+    
+    </div>
   )
 }
