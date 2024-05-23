9b824b7a6c0af56ed281bc2e4b6f49b5062d908e
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-05-23 11:09:19 +0200

    Multifunktionswebsite added more divs

--- REACT/SocialmediaApp/onurgram/src/components/qrCode.js
+++ REACT/SocialmediaApp/onurgram/src/components/qrCode.js
@@ -3,7 +3,9 @@ import QRCode from 'react-qr-code';
 export default function QrCode() {
   return (
     <div className='flex items-center justify-center w-full h-full'>
-      <div className='border border-black w-[500px] h-[500px]'><QRCode value='`WIFI:T:WPA;S:${Magenta};P:${ARSLAN6820};;`'/></div>
+      <div className='w-[600px] h-[200px] relative top-[420px]'></div>
+      <div className='flex justify-center items-center border border-black w-[600px] h-[600px] bg-white'><QRCode value='`WIFI:T:WPA;S:${Magenta};P:${ARSLAN6820};;`'/></div>
+      <div></div>
     </div>
   )
 }
