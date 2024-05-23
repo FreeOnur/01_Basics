d79cd6076c33b67a69bad7452254ba4d57b9a2b7
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-05-23 10:34:26 +0200

    Multifunktionswebsite changed some textures added some textures

--- REACT/SocialmediaApp/onurgram/src/components/qrCode.js
+++ REACT/SocialmediaApp/onurgram/src/components/qrCode.js
@@ -2,6 +2,8 @@ import QRCode from 'react-qr-code';
 
 export default function QrCode() {
   return (
-    <div className=''><QRCode value='`WIFI:T:WPA;S:${Magenta};P:${ARSLAN6820};;`'/></div>
+    <div className='flex items-center justify-center w-full h-full'>
+      <div className='border border-black w-[500px] h-[500px]'><QRCode value='`WIFI:T:WPA;S:${Magenta};P:${ARSLAN6820};;`'/></div>
+    </div>
   )
 }
