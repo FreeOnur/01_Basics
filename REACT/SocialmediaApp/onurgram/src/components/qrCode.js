c7c0a9f5da9767e5b4da0de608ec3e2934ad8147
BuildTools <unconfigured@null.spigotmc.org>
2024-05-30 22:27:57 +0200

    Multifunktionsapp finished qr code for WLAN and texture and the other functions of the generator

--- REACT/SocialmediaApp/onurgram/src/components/qrCode.js
+++ REACT/SocialmediaApp/onurgram/src/components/qrCode.js
@@ -6,9 +6,14 @@ export default function QrCode() {
   const [hidden, setHidden] = useState("");
   const [password, setPassword] = useState("");
   const [SSID, setSSID] = useState("");
-  const [bgColor, setBgColor] = useState("#FFFFFF")
-  const [fgColor, setFgColor] = useState("#00000")  
+  const [bgColor, setBgColor] = useState("#000000")
+  const [fgColor, setFgColor] = useState("#FFFFFF")  
   const [qrImg, setImg] = useState("")
+  const [qrSize, setQrSize] = useState(128)
+  const [centerSizeWidth, setCenterSizeWidth] = useState(24)
+  const [centerSizeHeight, setCenterSizeHeight] = useState(24)
+
+  
   
   let array1 = [wifiType, hidden ? `H:${hidden}` : "", SSID ? `S:${SSID}` : "",password ? `P:${password}` : ""];
 
@@ -17,33 +22,49 @@ export default function QrCode() {
      
       wifi += array1[i] + ";";
     
-  }
+  } 
   
-
- 
- 
   return (
     <div className='flex items-center justify-center w-full h-full'>
       <div className='relative left-[930px] top-[80px] h-[650px] w-[250px] pr-[30px] bg-[#191919] border border-black'>
         <div className=' grid grid-cols-2 grid-rows-4 gap-3'>
           <div className='font-poppins drop-shadow-[10px] text-shadow-white text-white antialiased'>Size:</div>
-          <input id='size' type="number" min="0"/>
+          <input id='size' onChange={(element) => {setQrSize(element.target.value);}} type="number" min="48"/>
           <div className='font-poppins drop-shadow-[10px] text-shadow-white text-white antialiased'>Colour-Bg:</div>
           <input id='qr-bg-color'  onChange={(element) => {setBgColor(element.target.value);}} type="color"/>
           <div className='font-poppins drop-shadow-[10px] text-shadow-white text-white antialiased'>Colour-pixel:</div>
           <input id='qr-pix-color' onChange={(element) => {setFgColor(element.target.value);}} type="color"/>
           <div className='font-poppins drop-shadow-[10px] text-shadow-white text-white antialiased'>Logo:</div>
-          <input id='qr-image' onChange={(e) => {
-            let file = e.target.files[0];
+          <input id='qr-image' onChange={(element) => {
+            let file = element.target.files[0];
             let reader = new FileReader;
             reader.onload = (e) => {
               setImg(e.target.result)
             }
             reader.readAsDataURL(file);
           }} className=' w-[127px]' type="file"/>
+          <div className='font-poppins drop-shadow-[10px] text-shadow-white text-white antialiased'>H:</div>
+          <input id='size' className='w-[30px]' onChange={(element) => {setCenterSizeHeight(element.target.value);}} type="number" min="0"/>
+          <div className='font-poppins drop-shadow-[10px] text-shadow-white text-white antialiased'>W:</div>
+          <input id='size' className='w-[30px]' onChange={(element) => {setCenterSizeWidth(element.target.value);}} type="number" min="0"/>
+          <button onClick={() => {
+            setWifiType("T:WPA")
+            setHidden("")
+            setPassword("")
+            setSSID("")
+            setBgColor("#000000")
+            setFgColor("#FFFFFF")
+            setImg("")
+            setQrSize(128)
+            setCenterSizeHeight(24)
+            setCenterSizeWidth(24)
+
+          }} className='font-poppins drop-shadow-[10px] text-shadow-white text-white antialiased border border-white ml-[20px]'>Reset</button>
+
        
           
         </div>
+        
       </div>
       <div className='grid grid-cols-1'>
     	    
@@ -83,12 +104,10 @@ export default function QrCode() {
         </div>
 
         <div className='flex justify-center items-center border border-black w-[650px] h-[650px] bg-white'>
-          <QRCode level='H' value={wifi} bgColor={bgColor} fgColor={fgColor} imageSettings={{
+          <QRCode level='H' size={qrSize} value={wifi} bgColor={bgColor} fgColor={fgColor} imageSettings={{
             src: qrImg,
-            x: undefined,
-            y: undefined,
-            height: 24,
-            width: 24,
+            height: centerSizeHeight,
+            width: centerSizeWidth,
             excavate: true,
           }}/>
         </div>
