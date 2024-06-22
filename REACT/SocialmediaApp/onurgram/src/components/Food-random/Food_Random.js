3a6f02337684a9a7f4906a18c66a0ae9a8ce57f8
BuildTools <unconfigured@null.spigotmc.org>
2024-06-22 22:44:38 +0200

    Multifunktionswebsite tried making sections

--- REACT/SocialmediaApp/onurgram/src/components/Food-random/Food_Random.js
+++ REACT/SocialmediaApp/onurgram/src/components/Food-random/Food_Random.js
@@ -11,8 +11,9 @@ export default function Food_Random() {
   return (
     <div className='size-[800px]'>
       <div>
-        <div id='wheel' className='h-[800px] border-8 border-white rounded-full ease-in-out' style={{ transform: `rotate(${rotation}deg)`, transition: 'transform 7s' }}>
-          <h1>hello</h1>
+        <div id='wheel' className='h-[800px] border-8 border-white rounded-full ease-in-out' style={{ transform: `rotate(${rotation}deg)`, transition: 'transform 7s'  }}>
+        <div class="absolute top-[50%] left-[50%] w-[390px] h-0.5 bg-black origin-left rotate-[30deg]"></div>
+        <div class="absolute top-[50%] left-[50%] w-[390px] h-0.5 bg-black origin-left rotate-[60deg]"></div>
         </div>
       </div>
     </div>
