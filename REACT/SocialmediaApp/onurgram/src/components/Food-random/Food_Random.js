98c32fc3a66d5b41c4fe41a4bbc2493b3f9cfb1f
FreeOnur <onur.arslan@student.htldornbirn.at>
2024-06-24 12:01:18 +0200

    Multifunktionswebsite Food Randomizer added button

--- REACT/SocialmediaApp/onurgram/src/components/Food-random/Food_Random.js
+++ REACT/SocialmediaApp/onurgram/src/components/Food-random/Food_Random.js
@@ -4,13 +4,19 @@ export default function Food_Random() {
   const [rotation, setRotation] = useState(0);
 
   useEffect(() => {
-    const randomRotation = Math.floor(Math.random() * 5 + 3) * 360 + Math.random() * 360;
+    const randomRotation = Math.floor(Math.random() * 900 + 12000)
     setRotation(randomRotation);
   }, []);
+
+  const clickRotation = () =>{
+    const randomRotation = Math.floor(Math.random() * 900 + 12000)
+    setRotation(randomRotation)
+  }
 
   return (
     <div className='size-[800px]'>
       <textarea className='absolute right-[0px] m-[100px] top-[30%]'></textarea>
+      <button onClick={clickRotation}>click me</button>
       <div>
         <div id='wheel' className='h-[800px] border-8 border-white rounded-full ease-in-out' style={{ transform: `rotate(${rotation}deg)`, transition: 'transform 7s'  }}>
         <div class="absolute top-[50%] left-[50%] w-[390px] h-0.5 bg-black origin-left rotate-[30deg]"></div>
